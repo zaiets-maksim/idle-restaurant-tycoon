e18e89319f9a149a3966b50bd554b955336029ff
zaiets-maksim <zaiets.maksim@gmail.com>
2026-07-11 03:02:33 +0300

    chore: lock FPS to 60, optimize Android settings, update scene

--- Assets/Scripts/Infrastructure/ProjectBootstrap.cs
+++ Assets/Scripts/Infrastructure/ProjectBootstrap.cs
@@ -10,7 +10,6 @@ namespace Infrastructure
         private static void Init()
         {
             Application.targetFrameRate = 60;
-            Application.fixedTimestep = 1f / 60f;
 
             var prefab = Resources.Load<ProjectContext>(ProjectContextPath);
             Object.Instantiate(prefab);
