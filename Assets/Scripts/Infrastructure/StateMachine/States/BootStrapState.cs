1b8aee57fe304e8f906a7cf86bf597a297854ef4
zaiets-maksim <zaiets.maksim@gmail.com>
2026-07-19 02:09:44 +0300

    fix: restore async scene loading in BootStrapState to ensure SceneLoader initialization before gameplay

--- Assets/Scripts/Infrastructure/StateMachine/States/BootStrapState.cs
+++ Assets/Scripts/Infrastructure/StateMachine/States/BootStrapState.cs
@@ -1,24 +1,29 @@
 using Infrastructure;
 using Infrastructure.StateMachine;
 using Infrastructure.StateMachine.States;
+using Services.SceneLoader;
 
 public class BootStrapState : GameStateEntity
 {
     private readonly IStateMachine _stateMachine;
+    private ISceneLoader _sceneLoader;
 
     public BootStrapState(ProjectContext projectContext)
     {
+        _sceneLoader = ProjectContext.Get<ISceneLoader>();
         _stateMachine = ProjectContext.Get<IStateMachine>();
     }
     
     public override void Enter()
     {
-        _stateMachine.Enter<LoadProgressState>();
+        _sceneLoader.Load(SceneTypeId.Loader, OnLevelLoad);
     }
 
     public override void OnLevelLoad()
     {
+        _stateMachine.Enter<LoadProgressState>();
     }
+
 
     public override void Tick()
     {
